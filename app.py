c76f834f593e11c1188532c71b066c866e370266
Charlie Jewers <charliejewers@charlies-mbp.home>
2019-09-15 15:27:56 +0100

    added expected goals functionality

--- app.py
+++ app.py
@@ -3,13 +3,18 @@ import xg_test
 
 app = Flask(__name__)
 
-#data to be displayed on index
-data = xg_test.get_xpts()
+#data to be displayed on pages
+form_data = xg_test.get_xpts()
+score_data = xg_test.get_xgoals()
 
 #index route
 @app.route('/')
 def index():
-    return render_template('index.html', overachievers = data[0], underachievers = data[1])
+    return render_template('index.html', pts_outliers=form_data, goals_outliers=score_data)
+
+@app.route('/players')
+def players():
+    return render_template('players.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- xg_test.py
+++ xg_test.py
@@ -1,39 +1,38 @@
-#Indicates which teams are over-achieving and under-achieving in the league. 
-
-#main package
+#stats package
 from understat import Understat
 
 #JSON and async packages
 import json
 import asyncio
-
-#async http
 import aiohttp
+
+#Maths packages
+import math
+import numpy as np
 
 
 #gets match data for every team in EPL
-async def main():
+async def fixture_data():
     async with aiohttp.ClientSession() as session:
         understat = Understat(session)
         results = await understat.get_teams(
             "epl", 2019
         )
-
     return results
 
 #compiles expected points for every team and classes them as over/underachieving
 def get_xpts():
     loop = asyncio.get_event_loop()
-    results = loop.run_until_complete(main())
+    results = loop.run_until_complete(fixture_data())
 
     #get team name and match data from table
     team_names = [team["title"] for team in results]
     team_history_dict = [team["history"] for team in results]
     match_history_dict = [match for match in team_history_dict]
 
+    #empty lists for over/underachievers
     overachievers = []
     underachievers = []
-    
 
     #sort teams and calculate full xPts for each
     for i in range(len(team_names)):
@@ -44,21 +43,73 @@ def get_xpts():
         #extract fixture history
         team_fixture_history = match_history_dict[i]
 
-        #get expected point total
-        x_pts = sum([fixture["xpts"] for fixture in team_fixture_history])
+        #list of expected point values
+        x_pts = [fixture["xpts"] for fixture in team_fixture_history]
+        x_pts_total = sum(x_pts)
 
         #get actual point total
-        real_pts = sum([fixture["pts"] for fixture in team_fixture_history])
+        real_pts_total = sum([fixture["pts"] for fixture in team_fixture_history])
 
-        pt_difference = real_pts - x_pts
+        #difference in real and expected points 
+        pt_difference = real_pts_total - x_pts_total
+
+        #compute chi-squared value 
+        chi_squared_points = (pt_difference**2)/x_pts_total
     
-        if pt_difference > 1:
-            overachievers.append((team_name, '%.3f'%(pt_difference)))
-        elif pt_difference < -1:
-            underachievers.append((team_name, '%.3f'%(pt_difference)))
+        #append to over/underachievers
+        if chi_squared_points > 0.5:
+            if pt_difference > 0:
+                overachievers.append((team_name, '%.3f'%(pt_difference)))
+            elif pt_difference < 0:
+                underachievers.append((team_name, '%.3f'%(pt_difference)))
             
     overachievers.sort(key=lambda x: x[1], reverse=True)
     underachievers.sort(key=lambda x: x[1], reverse=True)
 
     return (overachievers, underachievers)
 
+
+#calculate which teams are clinical/wasteful in attack
+def get_xgoals():
+    loop = asyncio.get_event_loop()
+    results = loop.run_until_complete(fixture_data())
+
+    #get team name and match data from table
+    team_names = [team["title"] for team in results]
+    team_history_dict = [team["history"] for team in results]
+    match_history_dict = [match for match in team_history_dict]
+    
+    #empty lists for over/underachievers
+    clinical = []
+    wasteful = []
+
+    #sort teams and calculate full xPts for each
+    for i in range(len(team_names)):
+
+        #get team name
+        team_name = team_names[i]
+
+        #extract fixture history
+        team_fixture_history = match_history_dict[i]
+
+        x_goals_total = sum([fixture["xG"] for fixture in team_fixture_history])
+        real_goals_total = sum([fixture["scored"] for fixture in team_fixture_history])
+        
+        goals_difference = real_goals_total - x_goals_total
+
+        #compute chi-squared value 
+        chi_squared_goals = (goals_difference**2)/x_goals_total
+
+        #append to over/underachievers if chi-squared exceeds critical value
+        if chi_squared_goals > 0.5:
+            if goals_difference > 0:
+                clinical.append((team_name, '%.3f'%(goals_difference)))
+            elif goals_difference < 0:
+                wasteful.append((team_name, '%.3f'%(goals_difference)))
+
+    clinical.sort(key=lambda x: x[1], reverse=True)
+    wasteful.sort(key=lambda x: x[1], reverse=True)
+
+    return (clinical, wasteful)
+
+# Functionality for expected goals against
